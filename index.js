7f31a652d3ee9d619fe0197b3b2f7fa24624a85a
Samuel Ackon <ackons433@gmail.com>
2022-02-01 08:20:31 +0000

    Update

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 /* eslint linebreak-style: ["error", "windows"] */
 /* eslint-disable no-plusplus */
 let toggleNavStatus = false;
+const getSpace = document.querySelector('.space');
+const getContainer = document.querySelector('.container');
+const getMenubar = document.querySelector('.firstmobile-nav');
+const getImages = document.querySelector('.images');
+const menuLinks = document.querySelectorAll('.firstmobile a');
+
+function bb() {
+  getMenubar.style.display = 'none';
+}
+
+function iterat() {
+  for (let i = 0; i < menuLinks.length; i++) {
+    menuLinks[i].addEventListener('click', bb);
+  }
+}
 
 function toggleNav() {
-  const getSpace = document.querySelector('.space');
-  const getContainer = document.querySelector('.container');
-  const getMenubar = document.querySelector('.firstmobile-nav');
-  const getImages = document.querySelector('.images');
+  for (let i = 0; i < menuLinks.length; i += 1) {
+    menuLinks[i].addEventListener('click', toggleNavStatus = true);
+  }
   if (toggleNavStatus === false) {
     getSpace.style.background = "url('./assets/menu/Group_98.png') 100% 100% no-repeat";
     getContainer.style.marginTop = '75vh';
@@ -17,12 +31,23 @@ function toggleNav() {
     toggleNavStatus = true;
   } else {
     getImages.style.background = '#fff';
-    // getSpace.style.background = "url('./assets/image_geometry_3.png') no-repeat";
     getSpace.style = document.querySelector('.footspace').sytle;
     getSpace.style.backgroundPosition = 'center center';
     getMenubar.style.display = 'none';
     getContainer.style.marginTop = 'inherit';
     toggleNavStatus = false;
   }
+
+  iterat();
 }
+
+// function myFunction() {
+//   const x = document.getElementById('myDIV');
+//   if (x.style.display === 'none') {
+//     x.style.display = 'block';
+//   } else {
+//     x.style.display = 'none';
+//   }
+// }
+
 document.getElementsByClassName('.firstmobile-nav').addEventListener('click', toggleNav);
