8a6fdebd5d082d9a6a76005187ca08be21e0beca
Samuel Ackon <ackons433@gmail.com>
2022-02-01 01:25:43 +0000

    Update

--- index.js
+++ index.js
@@ -3,11 +3,13 @@
 let toggleNavStatus = false;
 
 function toggleNav() {
-  const getBoxic = document.querySelector('.container');
+  const getSpace = document.querySelector('.space');
+  const getContainer = document.querySelector('.container');
   const getMenubar = document.querySelector('.firstmobile-nav');
   const getImages = document.querySelector('.images');
   if (toggleNavStatus === false) {
-    getBoxic.style.marginTop = '75vh';
+    getSpace.style.background = "url('./assets/menu/Group_98.png') 100% 100% no-repeat";
+    getContainer.style.marginTop = '75vh';
     getImages.style.background = '#e5e5e5';
     getMenubar.style.position = 'absolute';
     getMenubar.style.display = 'flex';
@@ -15,9 +17,12 @@ function toggleNav() {
     toggleNavStatus = true;
   } else {
     getImages.style.background = '#fff';
+    getSpace.style.width = '111px';
+    getSpace.style.height = '74.02px';
+    getSpace.style.background = "url('assets/image_geometry_3.png') 100% 100% no-repeat";
     getMenubar.style.display = 'none';
     toggleNavStatus = false;
-    getBoxic.style.marginTop = 'inherit';
+    getContainer.style.marginTop = 'inherit';
   }
 }
 document.getElementsByClassName('.firstmobile-nav').addEventListener('click', toggleNav);
