aab0c8418ce19044bc04111f89a33b2e58ae8468
Samuel Ackon <ackons433@gmail.com>
2022-02-01 00:02:37 +0000

    Update

--- index.js
+++ index.js
@@ -3,21 +3,22 @@
 let toggleNavStatus = false;
 
 function toggleNav() {
-  const getBoxic = document.querySelector('.ic-menu');
+  const getBoxic = document.querySelector('.container');
   const getMenubar = document.querySelector('.firstmobile-nav');
   const getImages = document.querySelector('.images');
   if (toggleNavStatus === false) {
+    getBoxic.style.marginTop = '90vh';
     getImages.style.background = '#e5e5e5';
     getMenubar.style.position = 'absolute';
     getMenubar.style.display = 'flex';
     getMenubar.style.width = '100%';
-    document.querySelector('.container')
-
+    
     toggleNavStatus = true;
   } else {
     getImages.style.background = '#fff';
     getMenubar.style.display = 'none';
     toggleNavStatus = false;
+    getBoxic.style.marginTop = 'inherit';
   }
 }
 document.getElementsByClassName('.firstmobile-nav').addEventListener('click', toggleNav);
