b59d905856f0b8cc60cb46920fe0fa824dee70e0
Samuel Ackon <ackons433@gmail.com>
2022-02-01 00:54:05 +0000

    Update

--- index.js
+++ index.js
@@ -7,12 +7,11 @@ function toggleNav() {
   const getMenubar = document.querySelector('.firstmobile-nav');
   const getImages = document.querySelector('.images');
   if (toggleNavStatus === false) {
-    getBoxic.style.marginTop = '90vh';
+    getBoxic.style.marginTop = '75vh';
     getImages.style.background = '#e5e5e5';
     getMenubar.style.position = 'absolute';
     getMenubar.style.display = 'flex';
     getMenubar.style.width = '100%';
-    
     toggleNavStatus = true;
   } else {
     getImages.style.background = '#fff';
