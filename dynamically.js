e87c717371445e34f39c827a77a0f8518bd6c858
Samuel Ackon <ackons433@gmail.com>
2022-02-02 15:48:39 +0000

    update

---
+++ dynamically.js
@@ -0,0 +1,26 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-console */
+const portfolioContainer = document.querySelector('#works');
+
+portfolioContainer.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  const modalToggle = e.target.closest('.project-view');
+
+  console.log('modalToggle');
+
+  //   if (!modalToggle) return;
+
+  //   const modal = modalToggle.parentNode;
+
+  //   const modalParent = modal.parentNode.previousElementSibling;
+  //   console.log(modalParent);
+  //   const closeButton = modalParent.querySelector('.closebox');
+
+  //   modalParent.classList.add('is-open');
+
+//   closeButton.addEventListener('click', (_) => {
+//     modalParent.classList.remove('is-open');
+//   });
+});
