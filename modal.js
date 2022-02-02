6aa34b90658839af09f854fe7f78e0c2f04e2509
Samuel Ackon <ackons433@gmail.com>
2022-02-02 12:33:57 +0000

    Update

--- modal.js
+++ modal.js
@@ -3,8 +3,20 @@
 const portfolioContainer = document.querySelector('#works');
 
 portfolioContainer.addEventListener('click', (e) => {
-  console.log(e);
+  e.preventDefault();
 
-  const modalToggle = e.target.closest('project-view');
-  console.log(modalToggle);
+  const modalToggle = e.target.closest('.project-view');
+
+  if (!modalToggle) return;
+
+  const modal = modalToggle.parentNode;
+  const modalParent = modal.parentNode.previousElementSibling;
+  console.log(modalParent);
+  const closeButton = modalParent.querySelector('.closebox');
+
+  modalParent.classList.add('is-open');
+
+  closeButton.addEventListener('click', (_) => {
+    modalParent.classList.remove('is-open');
+  });
 });
