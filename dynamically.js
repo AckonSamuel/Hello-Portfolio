cb1c74abe3894bf4e6c85db293c263db5e5caac5
Samuel Ackon <ackons433@gmail.com>
2022-02-02 23:23:59 +0000

    Update

--- dynamically.js
+++ dynamically.js
@@ -1,8 +1,19 @@
 /* eslint-disable linebreak-style */
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-console */
-const portfolioContainer = document.querySelector('#works');
-
+const portfolioContainer = document.querySelector('body');
+const modalBody = document.querySelector('.modal');
+const getchange = document.querySelector('.projetd');
+const getrool = document.getElementById('rool');
+const getProjectView = document.querySelector('.project-view');
+const cards = {
+  img2: './assets/work/work_two.png',
+  img3: 'assets/work/work_thee.png',
+  img4: 'assets/work/work_four.png',
+  img5: 'assets/work/work_five.png',
+  img6: 'assets/work/work_six.png',
+};
+getrool.innerHTML = '<img src="assets/work/work_two.png" class="work-one letgo" alt="first project image">';
 portfolioContainer.addEventListener('click', (e) => {
   e.preventDefault();
 
@@ -13,9 +24,8 @@ portfolioContainer.addEventListener('click', (e) => {
   const modal = modalToggle.parentNode.parentNode;
 
   const modalParent = modal.parentNode.parentNode.parentNode.firstChild;
-  console.log(modalParent);
-  const closeButton = modalParent.querySelector('.closebox');
 
+  const closeButton = modalParent.querySelector('.closebox');
   modalParent.classList.add('is-open');
 
   closeButton.addEventListener('click', (_) => {
