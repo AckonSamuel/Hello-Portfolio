d3310925649211cd294429bb78a7d87de03e2965
Samuel Ackon <ackons433@gmail.com>
2022-02-03 16:02:25 +0000

    Update on dynamic

--- dynamically.js
+++ dynamically.js
@@ -64,7 +64,216 @@ const AllButtons = document.getElementsByClassName('project-view');
 const work = document.querySelector('body');
 const popUp = document.createElement('section');
 popUp.classList.add('modalD');
+const prosec = document.querySelector('#works');
 
+const subzero = document.createElement('div');
+subzero.classList.add('project-name');
+subzero.style.display = 'block';
+prosec.appendChild(subzero);
+
+subzero.innerHTML = `
+<div class="project-name">
+<h2>Projects</h2>
+</div>`;
+
+const one = document.createElement('div');
+one.classList.add('work', 'one');
+one.style.display = 'block';
+prosec.appendChild(one);
+
+const two = document.createElement('div');
+two.classList.add('work', 'two');
+two.style.display = 'block';
+prosec.appendChild(two);
+
+const three = document.createElement('div');
+three.classList.add('work', 'three');
+three.style.display = 'block';
+prosec.appendChild(three);
+
+const four = document.createElement('div');
+four.classList.add('work', 'four');
+four.style.display = 'block';
+prosec.appendChild(four);
+
+const five = document.createElement('div');
+five.classList.add('work', 'five');
+five.style.display = 'block';
+prosec.appendChild(five);
+
+const six = document.createElement('div');
+six.classList.add('work', 'six');
+six.style.display = 'block';
+prosec.appendChild(six);
+
+const vreds = document.querySelector('.three');
+const vreds1 = document.querySelector('.four');
+const vreds2 = document.querySelector('.five');
+const vreds4 = document.querySelector('.one');
+const vreds5 = document.querySelector('.two');
+const vreds6 = document.querySelector('.six');
+
+vreds.style.display = 'inherit';
+vreds1.style.display = 'inherit';
+vreds2.style.display = 'inherit';
+
+// vreds4.style.display = 'inherit';
+vreds5.style.display = 'inherit';
+vreds.style.display = 'inherit';
+
+vreds.innerHTML = `
+                <div class="project photo"><img src=${projects[3].src} class="work-one"
+                        alt="project image three"></div>
+                <div class="project summary">
+                    <div class="namep">
+                        <h3>Project name goes here</h3>
+                    </div>
+                    <div class="technology">
+                        <ul class="hello">
+                            <li class="maybe">HTML/CSS</li>
+                            <li class="maybe">Ruby on rails</li>
+                            <li class="maybe">JavaScript</li>
+                        </ul>
+                    </div>
+                    <button class="project-view">
+                        See this project
+                        <img src="assets/work/Union.png" class="arrow" alt="#">
+                    </button>
+
+                </div>
+            </div>`;
+
+vreds1.innerHTML = `<div class="project photo"><img src=${projects[4].src} class="work-one"
+alt="project image four"></div>
+<div class="project summary">
+<div class="namep">
+<h3>Project name goes here</h3>
+</div>
+<div class="technology">
+<ul class="hello">
+    <li class="maybe">HTML/CSS</li>
+    <li class="maybe">Ruby on rails</li>
+    <li class="maybe">JavaScript</li>
+</ul>
+</div>
+<button class="project-view" id="card-4">
+See this project
+<img src="assets/work/Union.png" class="arrow" alt="#">
+</button>
+
+</div>`;
+
+vreds2.innerHTML = `
+<div class="project photo"><img src=${projects[5].src} class="work-one"
+                        alt="project image five"></div>
+                <div class="project summary">
+                    <div class="namep">
+                        <h3>Project name goes here</h3>
+                    </div>
+                    <div class="technology">
+                        <ul class="hello">
+                            <li class="maybe">HTML/CSS</li>
+                            <li class="maybe">Ruby on rails</li>
+                            <li class="maybe">JavaScript</li>
+                        </ul>
+                    </div>
+                    <button class="project-view" id="card-4">
+                        See this project
+                        <img src="assets/work/Union.png" class="arrow" alt="#">
+                    </button>
+
+                </div>`;
+
+vreds4.innerHTML = `
+<div class="project photo">
+<img src="${projects[0].src}" class="work-one" alt="first project image">
+</div>
+<div class="project summary desksum">
+<div class="namep namedesk">
+    <h3>Project name goes here</h3>
+</div>
+<div class="technology techdesk">
+    <ul class="hello hellodesk">
+        <li class="maybe">HTML/CSS</li>
+        <li class="maybe">Ruby on rails</li>
+        <li class="maybe">JavaScript</li>
+    </ul>
+</div>
+<button id="card-1" class="project-view viewdesk">
+    See this project
+    <img src="assets/work/Union.png" class="arrow arrowdesk" alt="#">
+</button>
+</div>
+`;
+vreds5.innerHTML = `
+<div class="option left"></div>
+                <div class="option middle"></div>
+                <div class="option right">
+                    <div class="man">
+                        <h3>Project name goes...</h3>
+                        <ul class="hellso">
+                            <li class="maybe">HTML/CSS</li>
+                            <li class="maybe">Ruby on rails</li>
+                            <li class="maybe">+1</li>
+                        </ul>
+                        <div class="being">
+                            <button id="card-2" class="project-view viewdesk">
+                                See this project
+                                <img src="assets/work/Union.png" class="arrow arrowdesk" alt="#">
+
+                            </button>
+                        </div>
+                    </div>
+
+                </div>
+                <div class="option rightdown"></div>
+                <div class="option leftdown"></div>
+                <div class="option cornerback"></div>
+                <div class="option lk"></div>
+                <div class="option blowman"></div>
+
+                <div class="project photo">
+                    <img src=${projects[1].src} class="work-one" alt="second project image">
+                </div>
+                <div class="project summary">
+                    <div class="namep">
+                        <h3>Project name goes here</h3>
+                    </div>
+                    <div class="technology">
+                        <ul class="hello">
+                            <li class="maybe">HTML/CSS</li>
+                            <li class="maybe">Ruby on rails</li>
+                            <li class="maybe">JavaScript</li>
+                        </ul>
+                    </div>
+                    <button class="project-view">
+                        See this project
+                        <img src="assets/work/Union.png" class="arrow" alt="#">
+
+                    </button>
+                </div>
+`;
+
+vreds6.innerHTML = `
+<div class="project photo"><img src=${projects[6].src} class="work-one"
+                        alt="project image five"></div>
+                <div class="project summary">
+                    <div class="namep">
+                        <h3>Project name goes here</h3>
+                    </div>
+                    <div class="technology">
+                        <ul class="hello">
+                            <li class="maybe">HTML/CSS</li>
+                            <li class="maybe">Ruby on rails</li>
+                            <li class="maybe">JavaScript</li>
+                        </ul>
+                    </div>
+                    <button class="project-view" id="card-4">
+                        See this project
+                        <img src="assets/work/Union.png" class="arrow" alt="#">
+                    </button>
+
+                </div>`;
 function popUpMobile(cardNumber) {
   popUp.style.display = 'block';
   work.appendChild(popUp);
