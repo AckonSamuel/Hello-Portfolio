b5c62f647aaa74503212e789dfeeeffda92f2cba
Samuel Ackon <ackons433@gmail.com>
2022-02-03 23:26:57 +0000

    Update validation code

--- validate.js
+++ validate.js
@@ -1,14 +1,15 @@
-const formButton = document.querySelector('#formButton');
-const userEmail = document.querySelector('#user_email');
-const errorMsg = document.querySelector('.form-error');
-errorMsg.textContent = 'Please add your email using lower case format * e.g. username@gmail.com*';
+/* eslint-disable linebreak-style */
+const formButton = document.querySelector('.collabo');
+const userEmail = document.querySelector('#email');
+const errorTxt = document.querySelector('.try-again');
+errorTxt.textContent = 'Please add your email using lower case format * e.g. username@gmail.com*';
 
 function emailValidation(e) {
   if (userEmail.value !== userEmail.value.toLowerCase()) {
-    errorMsg.style.display = 'block';
+    errorTxt.style.display = 'block';
     e.preventDefault();
   } else {
-    errorMsg.style.display = 'none';
+    errorTxt.style.display = 'none';
   }
 }
 
