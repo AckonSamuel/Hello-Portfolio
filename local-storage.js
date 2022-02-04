19a876cb84c472226cb37836634d21a083f3a8fc
beyk <beykmail@gmail.com>
2022-02-04 11:49:22 +0330

    Add logic to local-storage.js

--- local-storage.js
+++ local-storage.js
@@ -12,3 +12,16 @@ const comment = document.querySelector('#comment');
 
 const form = document.querySelector('form');
 
+if (localStorage.getItem('userData') != null) {
+  userData = JSON.parse(localStorage.getItem('userData'));
+  fullname.value = userData.fullname;
+  email.value = userData.email;
+  comment.value = userData.comment;
+}
+
+form.addEventListener('input', () => {
+  userData.fullname = fullname.value;
+  userData.email = email.value;
+  userData.comment = comment.value;
+  localStorage.setItem('userData', JSON.stringify(userData));
+});
