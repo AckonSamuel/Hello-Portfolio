00a67a3ce0548b5c699c04e3d89bbcd51ee01b81
Samuel Ackon <ackons433@gmail.com>
2022-02-04 08:15:38 +0000

    Set up local storage

---
+++ local-storage.js
@@ -0,0 +1,14 @@
+/* eslint-disable linebreak-style */
+
+let userData = {
+  fullname: '',
+  email: '',
+  comment: '',
+};
+
+const fullname = document.querySelector('#fullname');
+const email = document.querySelector('#email');
+const comment = document.querySelector('#comment');
+
+const form = document.querySelector('form');
+
